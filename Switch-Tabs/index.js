7b3c0284e7b9bd275bd34a8c9025a1681420815b
Roman <romantimilsina@gmail.com>
2023-01-30 14:15:07 +0545

    final

--- Switch-Tabs/index.js
+++ Switch-Tabs/index.js
@@ -8,6 +8,10 @@ function setupTabs(){
       const tabsContainer = sideBar.parentElement;
       const tabNumber = button.dataset.forTab;
       const tabsToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`)
+      sideBar.querySelectorAll('.tabs__button--active').forEach(b => {
+        b.classList.remove('tabs__button--active');
+      })
+      button.classList.add('tabs__button--active')
       document.querySelectorAll('.tabs__content').forEach(content => {content.style.display = 'none'; })
       tabsToActivate.style.display = 'block';
     })
